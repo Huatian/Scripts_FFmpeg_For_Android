dba4acaef65dda132138346e1813084558b3c6dd
WangHuatian <773512457@qq.com>
2017-02-11 14:35:01 +0800

    add x86

---
+++ build_android_x86.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+export TMPDIR=$HOME/temp
+NDK=$HOME/Android/Sdk/ndk-bundle
+SYSROOT=$NDK/platforms/android-19/arch-arm/
+TOOLCHAIN=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64
+
+CPU=x86
+PREFIX=$HOME/ffmpeg-android/x86/
+
+function build_one
+{
+./configure \
+  --prefix=$PREFIX \
+  --enable-shared \
+  --disable-static \
+  --disable-doc \
+  --disable-ffmpeg \
+  --disable-ffplay \
+  --disable-ffprobe \
+  --disable-ffserver \
+  --disable-doc \
+  --disable-symver \
+  --enable-small \
+  --disable-yasm \
+  --cross-prefix=$TOOLCHAIN/bin/i686-linux-android- \
+  --target-os=linux \
+  --arch=x86 \
+  --enable-cross-compile \
+  --sysroot=$SYSROOT \
+  --extra-cflags="-Os -fpic" \
+$ADDITIONAL_CONFIGURE_FLAG
+make clean
+make
+make install
+}
+
+build_one
